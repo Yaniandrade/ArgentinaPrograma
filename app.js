0513a51c6f6ae21223248f33e37bd66ee152d4d3
Yaniandrade <120784909+Yaniandrade@users.noreply.github.com>
2023-01-08 18:14:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
 app.use(express.static('public'));
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/home.html'))
+    res.sendFile(path.join(__dirname,'/views/index.html'))
 });
-app.listen(3030, () => {
-    console.log('Servidor iniciado en http://localhost:3030');
+
+app.use(express.static('public'));
+app.get('/productDetail',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/productDetail.html'))
 });
+app.get('/productCart',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/productCart.html'))
+});
+
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/register.html'))
+});
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/login.html'))
+});
+app.listen(3000, () => {
+    console.log('Servidor iniciado en http://localhost:3000');
+});
+
+
